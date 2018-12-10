9b13209574dbe4330f77325311df6174cb2c1ed3
Tudor Andreescu <tudor@parhelionsofware.com>
2018-12-10 16:46:28 +0200

    Initial Commit

---
+++ expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  expenses
+//
+//  Created by Danut Pralea on 10/12/2018.
+//  Copyright © 2018 TudorAndreescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
